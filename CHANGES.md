2847da1e3292355956b2ec14ff58a93bd1f0b5dd
Thomas Cyron <thomas@thcyron.de>
2019-02-07 15:44:53 +0100

    Release v1.1.0

---
+++ CHANGES.md
@@ -0,0 +1,9 @@
+# Changes
+
+## v1.1.0
+
+* Implement topology awareness (supporting nodes and volumes in different locations)
+
+## v1.0.0
+
+* Initial release
--- src/driver/driver.go
+++ src/driver/driver.go
@@ -2,7 +2,7 @@ package driver
 
 const (
 	PluginName    = "csi.hetzner.cloud"
-	PluginVersion = "1.0.0"
+	PluginVersion = "1.1.0"
 
 	MaxVolumesPerNode = 16
 	DefaultVolumeSize = 10 // GB
