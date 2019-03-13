329870070c290a71c4e6027962e817241b55e849
Thomas Cyron <thomas@thcyron.de>
2019-03-13 15:05:27 +0100

    Add script to update version

---
+++ script/updateversion.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+VERSION="$1"
+sed -i "" -e "s/image: hetznercloud\/hcloud-csi-driver:.*$/image: hetznercloud\/hcloud-csi-driver:$VERSION/g" deploy/kubernetes/*.yml
+sed -i "" -e "s/## master/## v$VERSION/g" CHANGES.md
+sed -i "" -e "s/PluginVersion = \".*\"$/PluginVersion = \"$VERSION\"/g" src/driver/driver.go
+goimports -w src/driver/driver.go
