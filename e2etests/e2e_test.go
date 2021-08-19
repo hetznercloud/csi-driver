29893dbb6581066e08f57609634975d6d70eec7b
Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>
2021-08-19 07:47:54 +0200

    Migrate Testsuite Setup to be in line with our CCM Testsuite (#219)
    Additionally, we now support/test on multi node clusters
    
    Signed-off-by: Lukas Kämmerling <lukas.kaemmerling@hetzner-cloud.de>

--- e2etests/e2e_test.go
+++ e2etests/e2e_test.go
@@ -1,106 +1,39 @@
 package e2etests
 
 import (
-	"context"
 	"fmt"
-	"math/rand"
 	"os"
-	"os/exec"
-	"strings"
 	"testing"
-	"time"
-
-	"github.com/hetznercloud/hcloud-go/hcloud"
 )
 
-var rng *rand.Rand
+var testCluster TestCluster
 
-func init() {
-	rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+func TestMain(m *testing.M) {
+	if err := testCluster.Start(); err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+
+	rc := m.Run()
+
+	if err := testCluster.Stop(rc > 0); err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(rc)
 }
-func TestCSIDriver(t *testing.T) {
-	fmt.Println("Starting CSI Driver Testsuite")
-	isUsingGithubActions := os.Getenv("GITHUB_ACTIONS")
-	isUsingGitlabCI := os.Getenv("CI_JOB_ID")
-	testIdentifier := ""
-	if isUsingGithubActions == "true" {
-		testIdentifier = fmt.Sprintf("gh-%s-%d", os.Getenv("GITHUB_RUN_ID"), rng.Int())
-		fmt.Println("Running in Github Action")
-	}
-	if isUsingGitlabCI != "" {
-		testIdentifier = fmt.Sprintf("gl-%s", isUsingGitlabCI)
-		fmt.Println("Running in Gitlab CI")
-	}
-	if testIdentifier == "" {
-		testIdentifier = fmt.Sprintf("local-%d", rng.Int())
-		fmt.Println("Running locally")
-	}
 
-	k8sVersion := os.Getenv("K8S_VERSION")
-	if k8sVersion == "" {
-		k8sVersion = "1.18.9"
-	}
-	token := os.Getenv("HCLOUD_TOKEN")
-	if len(token) != 64 {
-		t.Fatalf("No valid HCLOUD_TOKEN found")
-	}
-
-	var additionalSSHKeys []*hcloud.SSHKey
-
-	opts := []hcloud.ClientOption{
-		hcloud.WithToken(token),
-		hcloud.WithApplication("hcloud-csi-driver-testsuite", "1.0"),
-	}
-	hcloudClient := hcloud.NewClient(opts...)
-	additionalSSHKeysIdOrName := os.Getenv("USE_SSH_KEYS")
-	if additionalSSHKeysIdOrName != "" {
-		idsOrNames := strings.Split(additionalSSHKeysIdOrName, ",")
-		for _, idOrName := range idsOrNames {
-			additionalSSHKey, _, err := hcloudClient.SSHKey.Get(context.Background(), idOrName)
-			if err != nil {
-				t.Fatal(err)
-			}
-			additionalSSHKeys = append(additionalSSHKeys, additionalSSHKey)
+func TestOfficialTestsuite(t *testing.T) {
+	t.Run("parallel tests", func(t *testing.T) {
+		err := RunCommandVisibleOnServer(testCluster.setup.privKey, testCluster.setup.MainNode, fmt.Sprintf("KUBECONFIG=/root/.kube/config ./ginkgo -p -v -focus='External.Storage' -skip='\\[Feature:|\\[Disruptive\\]|\\[Serial\\]' ./e2e.test -- -storage.testdriver=test-driver.yml"))
+		if err != nil {
+			t.Error(err)
 		}
-
-	}
-
-	fmt.Printf("Test against k8s %s\n", k8sVersion)
-
-	fmt.Println("Building csi driver image")
-	cmd := exec.Command("docker", "build", "-t", fmt.Sprintf("hcloud-csi-driver:ci_%s", testIdentifier), "../")
-	cmd.Stdout = os.Stdout
-	err := cmd.Run()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	fmt.Println("Saving csi driver image to disk")
-	cmd = exec.Command("docker", "save", "--output", "ci-hcloud-csi-driver.tar", fmt.Sprintf("hcloud-csi-driver:ci_%s", testIdentifier))
-	cmd.Stdout = os.Stdout
-	err = cmd.Run()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	setup := hcloudK8sSetup{Hcloud: hcloudClient, K8sVersion: k8sVersion, TestIdentifier: testIdentifier, HcloudToken: token}
-	fmt.Println("Setting up test env")
-
-	err = setup.PrepareTestEnv(context.Background(), additionalSSHKeys)
-	if err != nil {
-		t.Fatalf("%s", err)
-	}
-
-	fmt.Println("Run tests")
-	err = setup.RunE2ETests()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	fmt.Println("Tear Down")
-	err = setup.TearDown(context.Background())
-	if err != nil {
-		t.Fatal(err)
-	}
-
+	})
+	t.Run("serial tests", func(t *testing.T) {
+		err := RunCommandVisibleOnServer(testCluster.setup.privKey, testCluster.setup.MainNode, fmt.Sprintf("KUBECONFIG=/root/.kube/config ./ginkgo -v -focus='External.Storage.*(\\[Feature:|\\[Serial\\])' ./e2e.test -- -storage.testdriver=test-driver.yml"))
+		if err != nil {
+			t.Error(err)
+		}
+	})
 }
