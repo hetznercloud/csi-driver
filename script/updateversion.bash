08bf43c7e73db33841cf89ca5c414adae0355888
Julian Tölle <julian.toelle@hetzner-cloud.de>
2022-11-21 10:09:36 +0100

    fix: driver version not updated on tagged release
    This version of the driver was not updated on new releases.
    This causes the user-agent to include the old version.

--- script/updateversion.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-VERSION="$1"
-sed -i "" -e "s/image: hetznercloud\/hcloud-csi-driver:.*$/image: hetznercloud\/hcloud-csi-driver:$VERSION/g" deploy/kubernetes/*.yml
-sed -i "" -e "s/## master/## v$VERSION/g" CHANGES.md
-sed -i "" -e "s/PluginVersion = \".*\"$/PluginVersion = \"$VERSION\"/g" driver/driver.go
-goimports -w driver/driver.go
