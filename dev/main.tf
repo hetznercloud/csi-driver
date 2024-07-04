b6033b3a808726665f8aa8241c59ea2f99bc78ac
jo <ljonas@riseup.net>
2024-07-04 16:11:40 +0200

    chore: update dev files

--- dev/main.tf
+++ dev/main.tf
@@ -1,8 +1,9 @@
 module "dev" {
-  source = "github.com/hetznercloud/terraform-k8s-dev?ref=v0.3.0"
+  source = "github.com/hetznercloud/kubernetes-dev-env?ref=v0.3.0"
 
   name         = "csi-driver-${replace(var.name, "/[^a-zA-Z0-9-_]/", "-")}"
   hcloud_token = var.hcloud_token
+  worker_count = 3
 
   k3s_channel = var.k3s_channel
 }
