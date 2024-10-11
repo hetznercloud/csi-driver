46919a90231d0be5ddeeaee7f971b337737fdbc9
lukasmetzner <lukas@metzner.io>
2024-10-11 16:42:27 +0200

    feat: Minimum supported kernel version for XFS
    If all nodes in your cluster run the same kernel version, the CSI driver will automatically determine the correct `mkfs.xfs` options.
    
    However, if your cluster has nodes with different kernel versions, older nodes may fail to mount volumes created by newer kernels. To avoid this issue, you can set a minimum kernel version via `xfsMinSupportedKernel`. This ensures that all nodes use the appropriate `mkfs.xfs` defaults for maximum compatibility.
    
    The following strings are valid versions, but only the major, minor, and patch components are considered. If a major, minor, or patch version is not provided, it is evaluated as `0`.

--- internal/volumes/xfs_utils.go
+++ internal/volumes/xfs_utils.go
@@ -20,10 +20,31 @@ type KernelVersion struct {
 	patch int
 }
 
-func getKernelVersion() (KernelVersion, error) {
+func ParseKernelVersion(versionString string) (*KernelVersion, error) {
+	releaseString := strings.Trim(versionString, "\x00")
+	parts := strings.Split(releaseString, "-")
+	mmpParts := strings.Split(parts[0], ".") // Major.Minor.Patch
+
+	versions := make([]int, 3)
+	for i, part := range mmpParts {
+		version, err := strconv.Atoi(part)
+		if err != nil {
+			return &KernelVersion{}, err
+		}
+		versions[i] = version
+	}
+
+	return &KernelVersion{
+		major: versions[0],
+		minor: versions[1],
+		patch: versions[2],
+	}, nil
+}
+
+func getKernelVersion() (*KernelVersion, error) {
 	var utsname syscall.Utsname
 	if err := syscall.Uname(&utsname); err != nil {
-		return KernelVersion{}, err
+		return &KernelVersion{}, err
 	}
 
 	data := make([]byte, 65)
@@ -34,40 +55,22 @@ func getKernelVersion() (KernelVersion, error) {
 		data[i] = byte(utsname.Release[i])
 	}
 
-	releaseString := strings.Trim(string(data), "\x00")
-	parts := strings.Split(releaseString, "-")
-	mmpParts := strings.Split(parts[0], ".") // Major.Minor.Patch
-
-	versions := make([]int, 3)
-	for i, part := range mmpParts {
-		version, err := strconv.Atoi(part)
-		if err != nil {
-			return KernelVersion{}, err
-		}
-		versions[i] = version
-	}
-
-	return KernelVersion{
-		major: versions[0],
-		minor: versions[1],
-		patch: versions[2],
-	}, nil
+	return ParseKernelVersion(string(data))
 }
 
-func (k *KernelVersion) IsNewerThan(b KernelVersion) bool {
-	if k.major > b.major {
-		return true
+func (k *KernelVersion) IsNewerThan(b *KernelVersion) bool {
+	if k.major != b.major {
+		return k.major > b.major
 	}
-	if k.minor > b.minor {
-		return true
+
+	if k.minor != b.minor {
+		return k.minor > b.minor
 	}
-	if k.patch > b.patch {
-		return true
-	}
-	return false
+
+	return k.patch > b.patch
 }
 
-func GetXFSConfigPath() string {
+func GetXFSConfigPath(current *KernelVersion) string {
 	files, err := os.ReadDir(XFSDefaultConfigsLocation)
 	if err != nil {
 		return ""
@@ -98,16 +101,11 @@ func GetXFSConfigPath() string {
 	}
 
 	sort.Slice(supportedVersions, func(i, j int) bool {
-		return supportedVersions[j].IsNewerThan(supportedVersions[i])
+		return supportedVersions[i].IsNewerThan(&supportedVersions[j])
 	})
 
-	current, err := getKernelVersion()
-	if err != nil {
-		return ""
-	}
-
 	for _, supported := range supportedVersions {
-		if current == supported || current.IsNewerThan(supported) {
+		if *current == supported || current.IsNewerThan(&supported) {
 			configName := fmt.Sprintf("lts_%d.%d.conf", supported.major, supported.minor)
 			return path.Join(XFSDefaultConfigsLocation, configName)
 		}
