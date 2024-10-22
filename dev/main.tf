0a3998709a4cc7e117a34dc83424acf52ed80e41
lukasmetzner <lukas.metzner@hetzner-cloud.de>
2024-10-22 13:46:02 +0200

    test: updated test server location

--- dev/main.tf
+++ dev/main.tf
@@ -4,6 +4,7 @@ module "dev" {
   name         = "csi-driver-${replace(var.name, "/[^a-zA-Z0-9-_]/", "-")}"
   hcloud_token = var.hcloud_token
   worker_count = 3
+  hcloud_location = "hel1"
 
   k3s_channel = var.k3s_channel
 }
