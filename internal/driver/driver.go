47827b407257eb5a606f64c9c58ac67ddcc5d983
lukasmetzner <lukas.metzner@hetzner-cloud.de>
2024-10-28 08:43:53 +0100

    refactor: Removed old label

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -9,6 +9,5 @@ const (
 	DefaultVolumeSize = MinVolumeSize
 
 	TopologySegmentLocation = PluginName + "/location"
-	IsRootServerLabel       = "instance.hetzner.cloud/is-root-server"
 	ProvidedByLabel         = "instance.hetzner.cloud/provided-by"
 )
