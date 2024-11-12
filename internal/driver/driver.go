96222d9c26702111d941ac802df75774563ed04f
Julian Tölle <julian.toelle@hetzner-cloud.de>
2024-11-12 10:21:18 +0100

    ci: switch to releaser-pleaser (#778)
    Switch to releaser-pleaser[0] for planning new releases.
    This adds the following new features:
    
    - We can easily cut pre-releases, these are always useful if we want to
    allow some customers to test features before making an official release
    or to validate changes made to the release CI workflows
    - We can add our own release notes to each release to better inform
    customers about the new features we added or about any deprecations.
    
    [0] https://github.com/apricote/releaser-pleaser

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -2,7 +2,7 @@ package driver
 
 const (
 	PluginName    = "csi.hetzner.cloud"
-	PluginVersion = "2.10.0" // x-release-please-version
+	PluginVersion = "2.10.0" // x-releaser-pleaser-version
 
 	MaxVolumesPerNode = 16
 	MinVolumeSize     = 10 // GB
