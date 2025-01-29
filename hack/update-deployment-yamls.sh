f56bdc876ca467a17bff0bddad2bb22e187fdedf
lukasmetzner <lukas.metzner@hetzner-cloud.de>
2025-01-29 15:09:07 +0100

    fix(helm-chart): render args empty if no condition applies

--- hack/update-deployment-yamls.sh
+++ hack/update-deployment-yamls.sh
@@ -3,7 +3,7 @@ set -ueo pipefail
 
 # Template the chart with pre-built values to get the legacy deployment files
 # Also remove labels that are Helm specific
-helm template hcloud-csi chart \
+helm template --kube-version v1.31.4 hcloud-csi chart \
   --namespace kube-system \
   --set metrics.enabled=true \
   --set controller.matchLabelsOverride.app=hcloud-csi-controller \
