94064390aefa2a00523595d44fd79e1d1b55ad7f
releaser-pleaser <>
2025-05-07 13:46:27 +0000

    chore(main): release v2.14.0

--- internal/driver/driver.go
+++ internal/driver/driver.go
@@ -2,7 +2,7 @@ package driver
 
 const (
 	PluginName    = "csi.hetzner.cloud"
-	PluginVersion = "2.13.0" // x-releaser-pleaser-version
+	PluginVersion = "2.14.0" // x-releaser-pleaser-version
 
 	MaxVolumesPerNode = 16
 	MinVolumeSize     = 10 // GB
